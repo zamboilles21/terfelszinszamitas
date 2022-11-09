985555928ac07c0cac4537948ed64b565e432b09
SzaboPeter20 <2001szabopeter@gmail.com>
2022-11-09 10:57:41 +0100

    software_added_to_repo

---
+++ Software/TerFelSzoft/TerFelSzoft/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace TerFelSzoft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combo_alakzatok = new System.Windows.Forms.ComboBox();
+            this.korabbi = new System.Windows.Forms.ListBox();
+            this.pctbx_alakzat = new System.Windows.Forms.PictureBox();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.pctbx_egyenlet = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbx_alakzat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbx_egyenlet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // combo_alakzatok
+            // 
+            this.combo_alakzatok.DropDownHeight = 150;
+            this.combo_alakzatok.DropDownWidth = 130;
+            this.combo_alakzatok.FormattingEnabled = true;
+            this.combo_alakzatok.IntegralHeight = false;
+            this.combo_alakzatok.ItemHeight = 13;
+            this.combo_alakzatok.Items.AddRange(new object[] {
+            "Háromszög",
+            "Négyzet",
+            "Trapéz",
+            "Téglalap",
+            "Paralelogramma",
+            "Rombusz",
+            "Deltoid",
+            "Kör",
+            "Henger",
+            "Gúla",
+            "Gömb"});
+            this.combo_alakzatok.Location = new System.Drawing.Point(12, 12);
+            this.combo_alakzatok.MaxDropDownItems = 12;
+            this.combo_alakzatok.Name = "combo_alakzatok";
+            this.combo_alakzatok.Size = new System.Drawing.Size(130, 21);
+            this.combo_alakzatok.TabIndex = 0;
+            this.combo_alakzatok.SelectedIndexChanged += new System.EventHandler(this.combo_alakzatok_SelectedIndexChanged);
+            // 
+            // korabbi
+            // 
+            this.korabbi.FormattingEnabled = true;
+            this.korabbi.Location = new System.Drawing.Point(668, 343);
+            this.korabbi.Name = "korabbi";
+            this.korabbi.Size = new System.Drawing.Size(120, 95);
+            this.korabbi.TabIndex = 1;
+            // 
+            // pctbx_alakzat
+            // 
+            this.pctbx_alakzat.ImageLocation = "";
+            this.pctbx_alakzat.Location = new System.Drawing.Point(148, 12);
+            this.pctbx_alakzat.Name = "pctbx_alakzat";
+            this.pctbx_alakzat.Size = new System.Drawing.Size(640, 307);
+            this.pctbx_alakzat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctbx_alakzat.TabIndex = 2;
+            this.pctbx_alakzat.TabStop = false;
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Location = new System.Drawing.Point(362, 415);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(75, 23);
+            this.btn_calc.TabIndex = 3;
+            this.btn_calc.Text = "Számítás";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
+            // 
+            // pctbx_egyenlet
+            // 
+            this.pctbx_egyenlet.Location = new System.Drawing.Point(149, 13);
+            this.pctbx_egyenlet.Name = "pctbx_egyenlet";
+            this.pctbx_egyenlet.Size = new System.Drawing.Size(171, 154);
+            this.pctbx_egyenlet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctbx_egyenlet.TabIndex = 5;
+            this.pctbx_egyenlet.TabStop = false;
+            this.pctbx_egyenlet.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pctbx_egyenlet);
+            this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.pctbx_alakzat);
+            this.Controls.Add(this.korabbi);
+            this.Controls.Add(this.combo_alakzatok);
+            this.Name = "Form1";
+            this.Text = "TérFelszín számítóka";
+            ((System.ComponentModel.ISupportInitialize)(this.pctbx_alakzat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbx_egyenlet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox combo_alakzatok;
+        private System.Windows.Forms.ListBox korabbi;
+        private System.Windows.Forms.PictureBox pctbx_alakzat;
+        private System.Windows.Forms.Button btn_calc;
+        private System.Windows.Forms.PictureBox pctbx_egyenlet;
+    }
+}
+
---
+++ Software/TerFelSzoft/TerFelSzoft/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerFelSzoft
+{
+    public partial class Form1 : Form
+    {
+        static List<Label> labelek = new List<Label>();
+        static List<TextBox> textboxok = new List<TextBox>();
+        static string alakzat = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calc_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void combo_alakzatok_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            alakzat = combo_alakzatok.SelectedItem.ToString();
+            for (int i = 0; i < labelek.Count; i++)
+            {
+                this.Controls.Remove(labelek[i]);
+                labelek.RemoveAt(i);
+            }
+            for (int i = 0; i < textboxok.Count; i++)
+            {
+                this.Controls.Remove(textboxok[i]);
+                textboxok.RemoveAt(i);
+            }
+            makeinputfields();
+        }
+
+        private void makeinputfields()
+        {
+            
+            
+            int textboxheight = 20;
+            int textboxwidth = 100;
+            
+            switch (alakzat)
+            {
+                case "Háromszög":
+                    for (int i = 0; i < 4; i++)
+                    {
+                        Label newlabeltriangle = new Label();
+                        newlabeltriangle.Location = new Point(combo_alakzatok.Location.X, combo_alakzatok.Location.Y + 180 + i * 30);
+                        newlabeltriangle.Height = textboxheight;
+                        newlabeltriangle.Width = textboxwidth;
+                        newlabeltriangle.Name = "lbl_triangle_" + i;
+                        this.Controls.Add(newlabeltriangle);
+                        labelek.Add(newlabeltriangle);
+                        switch (i)
+                        {
+                            case 0:
+                                newlabeltriangle.Text = "a oldal:";
+                                break;
+                            case 1:
+                                newlabeltriangle.Text = "b oldal:";
+                                break;
+                            case 2:
+                                newlabeltriangle.Text = "c oldal:";
+                                break;
+                            case 3:
+                                newlabeltriangle.Text = "Magasság:";
+                                break;
+                        }
+                        TextBox newtextboxtriangle = new TextBox();
+                        newtextboxtriangle.Location = new Point(newlabeltriangle.Location.X+textboxwidth, newlabeltriangle.Location.Y-3);
+                        newtextboxtriangle.Height = textboxheight;
+                        newtextboxtriangle.Width = textboxwidth;
+                        newtextboxtriangle.Name = "txtbx_triangle_" + i;
+                        newtextboxtriangle.Text = "Írja be az értéket ";
+                        this.Controls.Add(newtextboxtriangle);
+                        textboxok.Add(newtextboxtriangle);
+                    }
+                    pctbx_alakzat.Image = Properties.Resources.triangle;
+                    pctbx_alakzat.Width = 400;
+                    pctbx_alakzat.Height = 200;
+                    pctbx_alakzat.Location = new Point(combo_alakzatok.Location.X+380, combo_alakzatok.Location.Y);
+                    pctbx_egyenlet.Visible = true;
+                    pctbx_egyenlet.Image = Properties.Resources.triangle;
+
+
+                    break;
+                case "Négyzet":
+                    if (true)
+                    {
+                        Label newlabelsquare = new Label();
+                        newlabelsquare.Location = new Point(combo_alakzatok.Location.X, combo_alakzatok.Location.Y + 180 + 30);
+                        newlabelsquare.Height = textboxheight;
+                        newlabelsquare.Width = textboxwidth;
+                        newlabelsquare.Name = "lbl_square";
+                        newlabelsquare.Text = "a oldal:";
+                        this.Controls.Add(newlabelsquare);
+
+                        TextBox newtextbox = new TextBox();
+                        newtextbox.Location = new Point(newlabelsquare.Location.X + textboxwidth, newlabelsquare.Location.Y - 3);
+                        newtextbox.Height = textboxheight;
+                        newtextbox.Width = textboxwidth;
+                        newtextbox.Name = "txtbx_square";
+                        newtextbox.Text = "Írja be az értéket ";
+                        this.Controls.Add(newtextbox);
+
+                        pctbx_alakzat.Image = Properties.Resources.negyzet;
+                        pctbx_alakzat.Width = 300;
+                        pctbx_alakzat.Height = 300;
+                        pctbx_alakzat.Location = new Point(combo_alakzatok.Location.X + 380, combo_alakzatok.Location.Y);
+                        pctbx_egyenlet.Visible = true;
+                        pctbx_egyenlet.Image = Properties.Resources.negyzet;
+                    }
+                        
+
+
+
+                    break;
+                case "Trapéz":
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Label newlabeltrap = new Label();
+                        newlabeltrap.Location = new Point(combo_alakzatok.Location.X, combo_alakzatok.Location.Y + 180 + i * 30);
+                        newlabeltrap.Height = textboxheight;
+                        newlabeltrap.Width = textboxwidth;
+                        newlabeltrap.Name = "lbl_triangle_" + i;
+                        this.Controls.Add(newlabeltrap);
+                        switch (i)
+                        {
+                            case 0:
+                                newlabeltrap.Text = "a oldal:";
+                                break;
+                            case 1:
+                                newlabeltrap.Text = "b oldal:";
+                                break;
+                            case 2:
+                                newlabeltrap.Text = "c oldal:";
+                                break;
+                            case 3:
+                                newlabeltrap.Text = "d oldal:";
+                                break;
+                            case 4:
+                                newlabeltrap.Text = "Magasság:";
+                                break;
+                        }
+                        TextBox newtextboxtrap = new TextBox();
+                        newtextboxtrap.Location = new Point(newlabeltrap.Location.X + textboxwidth, newlabeltrap.Location.Y - 3);
+                        newtextboxtrap.Height = textboxheight;
+                        newtextboxtrap.Width = textboxwidth;
+                        newtextboxtrap.Name = "txtbx_triangle_" + i;
+                        newtextboxtrap.Text = "Írja be az értéket ";
+                        this.Controls.Add(newtextboxtrap);
+                    }
+                    pctbx_alakzat.Image = Properties.Resources.trap;
+                    pctbx_alakzat.Width = 400;
+                    pctbx_alakzat.Height = 200;
+                    pctbx_alakzat.Location = new Point(combo_alakzatok.Location.X + 380, combo_alakzatok.Location.Y);
+                    pctbx_egyenlet.Visible = true;
+                    pctbx_egyenlet.Image = Properties.Resources.trap;
+                    break;
+                case "Téglalap":
+                    break;
+                case "Paralelogramma":
+                    break;
+                case "Rombusz":
+                    break;
+                case "Deltoid":
+                    break;
+                case "Kör":
+                    break;
+                case "Henger":
+                    break;
+                case "Gúla":
+                    break;
+                case "Gömb":
+                    break;
+                
+            }
+        }
+    }
+}
